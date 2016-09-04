0d8806606f950f18960e54ef7392d069688fe151
Tanya <tny.cmd@gmail.com>
2016-09-04 16:37:00 -0400

    Add Concurrency in C Sharp project

---
+++ Concurrency_in_CSharp/AsynchronousProgrammingExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgrammingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSomethingAsync().Wait();
+
+            Console.WriteLine("Finsihed");
+            Console.ReadKey(); 
+        }
+
+        static async Task DoSomethingAsync()
+        {
+            int val = 13;
+
+            // Asynchronously wait 1 second
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            val *= 2;
+
+            // Asynchronously wait 1 second
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            // System.Diagnostics.Trace.WriteLine(val);
+            Console.WriteLine(val);
+
+        }
+    }
+}
---
+++ Concurrency_in_CSharp/Concurrency_in_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurrency_in_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
